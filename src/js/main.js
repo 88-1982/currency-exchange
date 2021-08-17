a59bf67b7d68f9db824d6dc7f5b3dd530700bff1
DeAunte Hall <75832818+88-1982@users.noreply.github.com>
2021-08-17 13:40:42 -0700

    update name of project in webpack.config and add data to dollar-conversion and main.js

---
+++ src/js/main.js
@@ -0,0 +1,52 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import DollarConversion from './services/conversion-service.js';
+import {convertCurrency} from './js/currency-functions.js';
+import {checkCurrency} from './js/currency-functions.js';
+
+//Checks to see if a number was inputted. Otherwise displays conversion in HTML.
+function emptyInput(inputAmount, fromCurrency, convertedAmount) {
+  if (inputAmount === "") {
+    $("#new-currency").append("Please input an amount");
+  } else {
+    $("#new-currency").append(`• <br> • <br> • <br><br> ${inputAmount} ${fromCurrency} = ${convertedAmount}`);
+  }
+}
+
+//Looks for any errors with 200 ok status and displays them in HTML.
+function checkAllErrors(conversionResponse) {
+  if (conversionResponse["error-type"]) {
+    $("#new-currency").append(`${conversionResponse.result} ${conversionResponse["error-type"]}<br>`);
+    return conversionResponse;
+  } else {
+    return conversionResponse;
+  }
+}
+
+$(document).ready(function () {
+  $(".container").slideDown("slow");
+  $("#currency-form").submit(async function (event) {
+    event.preventDefault();
+    $("#new-currency").empty();
+    let inputAmount = $("input#input-amount").val();
+    let fromCurrency = $("#from-currency").val();
+    let toCurrency = $("#to-currency").val();
+    try {
+      let conversionResponse = await DollarConversion.getConversion(fromCurrency);
+      if (conversionResponse === Error) {
+        throw Error();
+      } else {
+        checkAllErrors(conversionResponse);
+        const rateResult = checkCurrency(conversionResponse, toCurrency, fromCurrency);
+        let convertedAmount = convertCurrency(rateResult, inputAmount, toCurrency);
+        emptyInput(inputAmount, fromCurrency, convertedAmount);
+        $("#new-currency").slideDown("slow");
+      }
+    } catch (error) {
+      $("#new-currency").append(`${error["error-type"]}`);
+      $("#new-currency").slideDown("slow");
+    }
+  });
+});
